0ad9aea1683c168a507470b53d5e9f806a40a6e4
dayongzai <981661375@qq.com>
2018-04-27 19:04:10 +0800

    啊

--- app.js
+++ app.js
@@ -4,45 +4,112 @@ const io = new Server();
 //io.setheader("access-contro-allow-origin","*");
 //io.set("origins","*");
 //io.origins("*");
-io.listen('5000');
+io.listen('3000');
 
-var sendToClient = function (clientroom,event,type,data,callback=null) {
-    io.sockets.in(clientroom).emit(event,{type:type,data:data});
+var sendToClient = function (socket,clientroom,event,type,data,callback=null) {
+    //io.sockets.in(clientroom).emit(event,{type:type,data:data});
+    socket.broadcast.to(clientroom).emit(event,{type:type,data:data});
     if(callback){
         callback();
     }
 };
 
-var clientList = {};
+var clientList = {};   //存储所有room值，
+
 console.log("server run!");
 io.on('connect',function (client) {
 
-    console.log("there is one client connect");
 
-    client.on('joinRoom',(data)=>{          //加入相同房间
-        client.join(data.data);
-        clientList.client = data.data;
-        console.log("roomid = "+clientList.client);
-    });
+   console.log("there is one client connect");
 
+   var timeInterval=null;
 
+    var clientRoom=null;
     client.on('move',(data)=>{
-        sendToClient(clientList.client,'move',data.type,data.data);  //再次将客户端传来的数据发送给相同房间的客户端
-        console.log("已经收到客户端的移动信息");
+        sendToClient(client,clientRoom,'move',data.type,data.data);
+
     });
+
+    client.on('Particleactive',(data)=>{
+        sendToClient(client,clientRoom,'Particleactive',data.type,data.data);
+
+    });
+    client.on('game',(data)=>{
+        console.log('data.data = '+data.type);
+        if(data.type==='playerinfo'){
+            sendToClient(client,clientRoom,'game',data.type,data.data);
+            sendToClient(client,clientRoom,'showRivalInfo','','');
+        }else if(data.type==='gameover'){
+            io.sockets.in(clientRoom).emit("game",{type:"gameover",data:''})
+        }else if(data.type==='continuegame'){
+            sendToClient(client,clientRoom,'game',data.type,data.data);
+            //client.emit('showRivalInfo','');
+        }
+        else{
+            sendToClient(client,clientRoom,'game',data.type,data.data);
+
+        }
+
+    });
+
+    client.on('room',(data)=>{    //给链接的客户端分配room值  ，
+        clientRoom = data.data;
+        if(clientList.hasOwnProperty(clientRoom)){
+            if(data.type==='joinRoom'){
+                if( clientList[clientRoom]===1){
+                    client.join(clientRoom);
+                    clientList[clientRoom] = 2;
+                    client.emit('roomJudge','intoRoom');
+                    io.sockets.in(clientRoom).emit('game',{type:'allPlayerJoin',data:''});
+                    timeInterval= setInterval(()=>{
+                        io.sockets.in(clientRoom).emit('birdMove',{x_Percent:Math.random()*0.5,y_Percent:Math.random()*1,changeTime:(1-Math.random())*3+0.1});
+                    },1000);
+
+                }else{
+                    client.emit('roomJudge','roomNotRight');
+                }
+
+
+            }else if(data.type==='createRoom'){
+                client.emit('roomJudge','roomExist');
+            }
+        }else {
+            if(data.type==='joinRoom'){
+                client.emit('roomJudge','roomNotExist');
+            }else if(data.type==='createRoom'){
+                client.join(clientRoom);
+                clientList[clientRoom] = 1;           //将传进来的room值作为clientlist的键
+                console.log(clientList);
+                client.emit('roomJudge','intoRoom');
+            }
+        }
+    });
+
+    client.on("fireResult",(data)=>{
+        if(data.type==='catchBird'){
+            sendToClient(client,clientRoom,'fireResult',data.type,data.data);
+            io.sockets.in(clientRoom).emit("birdBeCatch",data.data);
+        }else if(data.type==='outBound'){
+            sendToClient(client,clientRoom,'fireResult',data.type,data.data);
+        }
+    });
+
+
+
+
 
 
     client.on("disconnect",function(data){
-        // clientList.indexOf(client);
-        // clientList.splice(clientList.indexOf(client),clientList.indexOf(client)+1);
-
-
-
-    });
-    client.on('touchmove',(data)=>{
-
-       client.emit("movelabel",data);  //要一并发送给自己才行
-       client.to("firstRoom").emit("movelabel",data);
+        // if(clientList[clientRoom]===2){
+        //     clientList[clientRoom]=1
+        //     sendToClient(client,clientRoom,'game','backToStartLevel','');
+        // }if(clientList[clientRoom]===1){
+        //     delete clientList[clientRoom];
+        //     sendToClient(client,clientRoom,'game','quitgame','');
+        // }
+        sendToClient(client,clientRoom,'game','quitgame','');
+        delete clientList[clientRoom];
+        clearInterval(timeInterval);   //必须清除定时器！！！！
     });
 
 });
